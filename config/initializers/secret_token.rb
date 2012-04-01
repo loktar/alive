3f9fd5df926e25c9079d692cc7ec40694b439380
Ian Fisher <i@taptera.com>
2012-04-01 16:55:22 -0700

    Generated a new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Alive::Application.config.secret_token = 'ad94cf4a7fdb5963e38ed37f83a69aeb21ab84d4f5d7c1ace70d5e6514ea4c7202cca162103e476d6f0610ee67e2e7f513bba7c1ef2c11e2c0eb355d1f87426a'
