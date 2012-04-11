01fd00c23bf616de976430ecb1550e47445ed90e
Ian Fisher <i@taptera.com>
2012-04-10 18:24:54 -0700

    Commented out puts statements

--- app/helpers/herbivore_helper.rb
+++ app/helpers/herbivore_helper.rb
@@ -9,11 +9,11 @@ module HerbivoreHelper
 
   def self.starve_with_tile(tile)
     desired_food = tile.herbivore_count * HERBIVORE_MEAL_SIZE
-    puts "herbs want to eat #{desired_food} and there are #{tile.herbivore_count} of them"
+    #puts "herbivores want to eat #{desired_food} and there are #{tile.herbivore_count} of them"
     if tile.life_amount < desired_food
       food_deficit = desired_food - tile.life_amount
       animals_to_starve = (1.0 * food_deficit / HERBIVORE_MEAL_SIZE).floor
-      puts "#{animals_to_starve} will starve"
+      #puts "#{animals_to_starve} will starve"
       tile.herbivore_count = [tile.herbivore_count - animals_to_starve, 0].max
     end
   end
@@ -22,16 +22,16 @@ module HerbivoreHelper
     old = tile.life_amount
     tile.life_amount = [tile.life_amount - (tile.herbivore_count * HERBIVORE_MEAL_SIZE), 0].max
 
-    puts "Consume from #{old} to #{tile.life_amount}"
+    #puts "Consume from #{old} to #{tile.life_amount}"
   end
 
   def self.reproduce_with_tile(tile)
-    puts "life amount: #{tile.life_amount}"
+    #puts "life amount: #{tile.life_amount}"
     if tile.life_amount > 0.5 && Random.rand < 0.3
       tile.herbivore_count = tile.herbivore_count + 1
-      puts "new herb count: #{tile.herbivore_count}"
+      #puts "new herbivore count: #{tile.herbivore_count}"
     else
-      puts "not adding herbs"
+      #puts "not adding herbivores"
     end
   end
 
