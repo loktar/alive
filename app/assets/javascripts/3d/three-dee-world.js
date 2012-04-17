e53effd00095809733bbde8efe08853a8c5361b1
Ian Fisher <i@taptera.com>
2012-04-16 23:18:14 -0700

    Dynamically pull in world updates for the 3D world

--- app/assets/javascripts/3d/three-dee-world.js
+++ app/assets/javascripts/3d/three-dee-world.js
@@ -5,6 +5,9 @@
   var TILE_SIZE = 100;
   var ENTITY_SIZE = 10;
 
+  var ADDED = 0;
+  var DELETED = 0;
+
   window.ThreeDeeWorld = function () {
     this.init();
     this.animate();
@@ -13,6 +16,8 @@
 
   window.ThreeDeeWorld.prototype = {
     init:function () {
+      this.entities = {};
+
       this.scene = new THREE.Scene();
 
       this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
@@ -50,6 +55,8 @@
     },
 
     addTilesToScene:function (tiles) {
+      ADDED = DELETED = 0;
+
       for (var i = 0; i < tiles.length; i++) {
         var tile = tiles[i];
         var geometry = new THREE.PlaneGeometry(TILE_SIZE, TILE_SIZE, 1, 1);
@@ -66,19 +73,52 @@
         this.addEntitiesToScene(tile.herbivores, 0xd1b38b, tileX, tileY);
         this.addEntitiesToScene(tile.carnivores, 0xbb0000, tileX, tileY);
       }
+
+      this.removeDeletedEntities();
+
+      console.log("added: " + ADDED + "; deleted: " + DELETED);
+    },
+
+    removeDeletedEntities:function () {
+      var entityIdsToDelete = [];
+      for (var entityId in this.entities) {
+        if (this.entities.hasOwnProperty(entityId)) {
+          var entityInfo = this.entities[entityId];
+          if (!entityInfo.justAdded) {
+            entityIdsToDelete.push(entityId);
+          }
+          entityInfo.justAdded = false;
+        }
+      }
+
+      for (var j = 0; j < entityIdsToDelete.length; j++) {
+        var idToDelete = entityIdsToDelete[j];
+
+        var deletedMesh = this.entities[idToDelete].mesh;
+        this.scene.remove(deletedMesh);
+
+        delete this.entities[idToDelete];
+        DELETED++;
+      }
     },
 
     addEntitiesToScene:function (entities, color, tileX, tileY) {
       $.each(entities, function (i, entity) {
-        var geometry = new THREE.CubeGeometry(ENTITY_SIZE, ENTITY_SIZE, ENTITY_SIZE);
-        var material = new THREE.MeshLambertMaterial({ color:color, wireframe:WIREFRAME });
+        if (!this.entities[entity.id]) {
+          var geometry = new THREE.CubeGeometry(ENTITY_SIZE, ENTITY_SIZE, ENTITY_SIZE);
+          var material = new THREE.MeshLambertMaterial({ color:color, wireframe:WIREFRAME });
 
-        var mesh = new THREE.Mesh(geometry, material);
-        var x = tileX + ((entity.x - 0.5) * TILE_SIZE);
-        var y = tileY + ((entity.y - 0.5) * TILE_SIZE);
-        mesh.position.set(x, y, ENTITY_SIZE / 2);
+          var mesh = new THREE.Mesh(geometry, material);
+          var x = tileX + ((entity.x - 0.5) * TILE_SIZE);
+          var y = tileY + ((entity.y - 0.5) * TILE_SIZE);
+          mesh.position.set(x, y, ENTITY_SIZE / 2);
 
-        this.scene.add(mesh);
+          this.scene.add(mesh);
+
+          this.entities[entity.id] = {mesh:mesh, justAdded:true};
+          ADDED++;
+        }
+        this.entities[entity.id].justAdded = true;
       }.bind(this));
     },
 
@@ -88,10 +128,11 @@
         url:'/worlds/current.json',
         success:function (response) {
           self.addTilesToScene(response.tiles);
+
+          setTimeout(self.fetchTiles.bind(self), 2000);
         }
       })
     }
   };
-
 
 }(jQuery));
